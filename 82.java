eef2e4433789b958b596b3a9a5a23ae9a163e805
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-09-01 14:59:44 +0530

    Create 82.java

---
+++ 82.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		float l,b,a;
+		Scanner s=new Scanner(System.in);
+		l=s.nextFloat();
+		b=s.nextFloat();
+		a=l*b;
+		System.out.format("%.5f",a);
+	}	
+}
+		
+	
